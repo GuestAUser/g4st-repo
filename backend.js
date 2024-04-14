09d8b9d038e33ed268c7f042f0bc204e26b30f3b
Lk10 <62957566+GuestAUser@users.noreply.github.com>
2024-04-14 16:01:52 -0300

    Add files via upload

---
+++ backend.js
@@ -0,0 +1,13 @@
+document.addEventListener(
+    'DOMContentLoaded',
+    function () {
+      document.querySelectorAll('.rain').forEach(
+        rain => {
+          rain.style.left = Math.random() * 100 + '%';
+          rain.style.right = Math.random() * 100 + '%';
+          rain.style.animationDuration = Math.random() * 1 + 1 + 's';
+          rain.style.animationDelay = Math.random() * 2 + 's';
+        }
+      );
+    }
+  );
